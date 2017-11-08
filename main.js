df7dd7abd9047ff74146f308cb3447a88e0b062a
Jeremy <jeremyxu123@gmail.com>
2017-11-08 13:48:33 -0800

    animations tutorial

---
+++ main.js
@@ -0,0 +1,14 @@
+function changeWidth(element) {
+  let elementClass = element.className;
+  if (elementClass.match("wide")) {
+    element.className = elementClass.replace(" wide", "");
+  } else {
+    element.className += " wide";
+  }
+}
+
+
+let box1 = document.querySelector(".box1");
+box1.addEventListener("click", function() {
+  changeWidth(box1);
+})
